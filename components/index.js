75eb847bc15d9407827fe16db96bebb840e485a0
juliusw01 <95189236+juliusw01@users.noreply.github.com>
2022-12-11 18:21:16 +0100

    Add files via upload

---
+++ components/index.js
@@ -0,0 +1,11 @@
+import HomeHeader from "./HomeHeader";
+import FocusedStatusBar from "./FocusedStatusBar";
+import Card from "./Card";
+import { CircleButton, RectButton } from "./Button";
+import DetailsBid from "./DetailsBid";
+import DetailsDesc from "./DetailsDesc";
+import { SubInfo } from "./SubInfo";
+
+export {
+    HomeHeader, Card, FocusedStatusBar, CircleButton, RectButton, DetailsBid, DetailsDesc, SubInfo
+}
